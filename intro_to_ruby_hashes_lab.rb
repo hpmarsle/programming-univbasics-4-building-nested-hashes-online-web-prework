0a9203e2daf695686f542a1b21dd6f8dc4697051
pragmatic-memory-1672 <hp.mars.le@gmail.com>
2019-07-23 19:13:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,10 @@ def monopoly_with_third_tier
 	  :names => {
 	    :reading_railroad => {
 	      
-	    } 
+	    },
+	    :pennsylvania_railroad => {
+	      
+	    }
 	  },
 	  :rent_in_dollars => {
 	    :one_piece_owned => 25,
